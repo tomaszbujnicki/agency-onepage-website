5052c71c8e53eb74f920dbcd94d10ea8fe0155d6
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2020-05-06 20:34:27 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+  $(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    /* autoplay: true, */
+    center: true,
+    loop: true,
+    nav: true,
+    navText: [
+        '<span></span>',
+        '<span></span>'
+    ]
+  });
+});
