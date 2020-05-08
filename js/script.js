02315ce004aba6db80357946aa1b37971e1c8171
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2020-05-08 23:45:57 +0200

    refactoring

--- js/script.js
+++ js/script.js
@@ -1,10 +1,43 @@
 
   $(document).ready(function(){
   $('.owl-carousel').owlCarousel({
-    /* autoplay: true, */
+    autoplay: false,
     center: true,
     loop: true,
-    nav: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        768:{
+            items:3,
+            nav: true
+        }
+    },
+    navText: [
+        '<span></span>',
+        '<span></span>'
+    ]
+  });
+});
+
+$(document).resize(function(){
+  $('.owl-carousel').owlCarousel({
+    autoplay: false,
+    center: true,
+    loop: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+            nav:false
+        },
+        768:{
+            items:3,
+            nav: true
+        }
+    },
     navText: [
         '<span></span>',
         '<span></span>'
