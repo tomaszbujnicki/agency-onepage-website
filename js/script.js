4d289d4bd593d58ff233f4a7a21fd2bfea7dd8b0
Tomasz Bujnicki <tomek.bujnicki@gmail.com>
2020-05-11 20:10:00 +0200

    add new section

--- js/script.js
+++ js/script.js
@@ -21,26 +21,3 @@
     ]
   });
 });
-
-$(document).resize(function(){
-  $('.owl-carousel').owlCarousel({
-    autoplay: false,
-    center: true,
-    loop: true,
-    responsiveClass:true,
-    responsive:{
-        0:{
-            items:1,
-            nav:false
-        },
-        768:{
-            items:3,
-            nav: true
-        }
-    },
-    navText: [
-        '<span></span>',
-        '<span></span>'
-    ]
-  });
-});
